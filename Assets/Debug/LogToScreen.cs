e3d1fdac937bbf5df5099dfd57ac990dc2b79de1
agalloch21 <agalloch21@gmail.com>
2024-10-15 09:41:13 +0800

    optimize effect

--- Assets/Debug/LogToScreen.cs
+++ Assets/Debug/LogToScreen.cs
@@ -6,6 +6,10 @@ public class LogToScreen : MonoBehaviour
 
     public TextMeshProUGUI text;
     public Transform offsetCamera;
+    public Transform mainCamera;
+    public Transform blackCamera;
+    public Transform stereoTrackedPose;
+    public Transform centralEyePose;
     public Transform plane;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +20,12 @@ public class LogToScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        text.text = $"Offset Camera: Pos:{offsetCamera.position}, Angles:{offsetCamera.eulerAngles} \n Plane: Pos:{plane.position}, Angles:{plane.eulerAngles}";
+        text.text = $"Offset Camera: Pos:{offsetCamera.position}, Angles:{offsetCamera.eulerAngles} \n " +
+            $"mainCamera: Pos:{mainCamera.position}, Angles:{mainCamera.eulerAngles}\n" +
+            $"blackCamera: Pos:{blackCamera.position}, Angles:{blackCamera.eulerAngles}\n" +
+            $"stereoTrackedPose: Pos:{stereoTrackedPose.position}, Angles:{stereoTrackedPose.eulerAngles}\n" +
+            $"centralEyePose: Pos:{centralEyePose.position}, Angles:{centralEyePose.eulerAngles}\n" +            
+            $"Plane: Pos:{plane.position}, Angles:{plane.eulerAngles}\n";
 
     }
 }
--- Assets/Scripts/PlanePlacer.cs
+++ Assets/Scripts/PlanePlacer.cs
@@ -7,24 +7,49 @@ public class PlanePlacer : MonoBehaviour
     public Camera monoCamera;
     public Camera stereoCamera;
 
+    Vector2 screenResoluion;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        screenResoluion = new Vector2(Screen.width, Screen.height);
+        MatchPlaneToScreenSize();
     }
 
-    // Update is called once per frame
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Camera cam = m_HoloKitCameraManager.ScreenRenderMode == ScreenRenderMode.Mono ? monoCamera : stereoCamera;
+
+    //    float pos = (cam.nearClipPlane + 10.0f);
+
+    //    transform.position = cam.transform.position + cam.transform.forward * pos;
+    //    //transform.forward = cam.transform.forward;
+    //    transform.LookAt(cam.transform);
+    //    float h = (Mathf.Tan(cam.fieldOfView * Mathf.Deg2Rad * 0.5f) * pos * 2f) * cam.aspect / 10.0f;
+    //    float w = h * Screen.height / Screen.width;
+    //    transform.localScale = new Vector3(h, w, 1) * 10;
+    //}
+
     void Update()
+    {
+        //if(screenResoluion.x != Screen.width || screenResoluion.y != Screen.height)
+        //{
+        //    MatchPlaneToScreenSize();
+        //    screenResoluion.x = Screen.width;
+        //    screenResoluion.y = Screen.height;
+        //}
+        MatchPlaneToScreenSize();
+    }
+
+    void MatchPlaneToScreenSize()
     {
         Camera cam = m_HoloKitCameraManager.ScreenRenderMode == ScreenRenderMode.Mono ? monoCamera : stereoCamera;
 
-        float pos = (cam.nearClipPlane + 10.0f);
+        float planeToCameraDistance = Vector3.Distance(gameObject.transform.position, cam.transform.position);
+        float planeHeightScale = (2.0f * Mathf.Tan(0.5f * cam.fieldOfView * Mathf.Deg2Rad) * planeToCameraDistance) / 10.0f;
+        float planeWidthScale = planeHeightScale * cam.aspect;
 
-        transform.position = cam.transform.position + cam.transform.forward * pos;
-        transform.forward = cam.transform.forward;
-        //transform.LookAt(cam.transform);
-        float h = (Mathf.Tan(cam.fieldOfView * Mathf.Deg2Rad * 0.5f) * pos * 2f) * cam.aspect / 10.0f;
-        float w = h * Screen.height / Screen.width;
-        transform.localScale = new Vector3(h, w, 1) * 10;
+        gameObject.transform.localScale = new Vector3(planeWidthScale, 1, planeHeightScale);
     }
 }
